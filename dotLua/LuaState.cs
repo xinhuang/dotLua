e92e22138cb7a8acb59e9a92f4223e8edb0d12f5
Patz Huang <walkaniceday@gmail.com>
2013-01-26 15:38:35 +0800

    Create class wrapping lua state.

---
+++ dotLua/LuaState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace dotLua
+{
+    public class LuaState : IDisposable
+    {
+        private readonly IntPtr _luaState = luaL_newstate();
+
+        [DllImport("Lua.dll")]
+        private static extern IntPtr luaL_newstate();
+
+        [DllImport("Lua.dll")]
+        private static extern void lua_close(IntPtr luaState);
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private void Dispose(bool isDisposing)
+        {
+            lua_close(_luaState);
+        }
+
+        ~LuaState()
+        {
+            Dispose(false);
+        }
+    }
+}
