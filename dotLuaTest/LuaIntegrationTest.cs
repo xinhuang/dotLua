96626188306a5e0ae2d36419d25f80dfe74a47b1
Patz Huang <walkaniceday@gmail.com>
2013-02-06 14:55:12 +0800

    Move demo to integration test project.

---
+++ dotLuaTest/LuaIntegrationTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using dotLua;
+
+namespace dotLuaTest
+{
+    [TestClass]
+    public class LuaIntegrationTest
+    {
+        private dynamic _sut;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _sut = new Lua();
+            _sut.Do("test.lua");
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _sut.Dispose();
+        }
+
+        [TestMethod]
+        public void calling_lua_function_without_arguments()
+        {
+            _sut.say_hi();
+        }
+    } 
+}
