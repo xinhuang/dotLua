05fab5e81858719f3193418b12c902de8af7c034
Patz Huang <walkaniceday@gmail.com>
2013-02-06 15:06:00 +0800

    Move all tests in demo to test project.

--- dotLuaTest/LuaIntegrationTest.cs
+++ dotLuaTest/LuaIntegrationTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using dotLua;
 
 namespace dotLuaTest
@@ -25,6 +27,71 @@ namespace dotLuaTest
         public void calling_lua_function_without_arguments()
         {
             _sut.say_hi();
+        }
+
+        [TestMethod]
+        public void calling_lua_function_with_1_argument()
+        {
+            _sut.say_hi("dotLua");
+        }
+
+        [TestMethod]
+        public void calling_lua_function_with_3_argument()
+        {
+            _sut.say_hi(Math.PI, "dotLua", "Sunday");
+        }
+
+        [TestMethod]
+        public void given_query_global_double_var_in_script_should_return_correct_value()
+        {
+            Assert.AreEqual(3, _sut.GlobalNumber);
+        }
+
+        [TestMethod]
+        public void given_query_global_string_var_in_script_should_return_correct_value()
+        {
+            Assert.AreEqual("Good luck~", _sut.GlobalString);
+        }
+
+        [TestMethod]
+        public void given_query_global_boolean_var_in_script_should_return_correct_value()
+        {
+            Assert.IsTrue(_sut.GlobalBoolean);
+        }
+
+        [TestMethod]
+        public void given_calling_lua_function_returning_1_double_should_result_be_received()
+        {
+            IList<dynamic> result0 = _sut.return_0();
+
+            Assert.AreEqual(1, result0.Count);
+            Assert.AreEqual(0, result0[0]);
+        }
+
+        [TestMethod]
+        public void given_calling_lua_function_returning_2_double_should_result_be_received()
+        {
+            IList<dynamic> result01 = _sut.return_0_1();
+
+            Assert.AreEqual(2, result01.Count);
+            Assert.AreEqual(0, result01[0]);
+            Assert.AreEqual(1, result01[1]);
+        }
+
+        [TestMethod]
+        public void given_calling_lua_function_returning_double_and_string_should_result_be_received()
+        {
+            IList<dynamic> result0str = _sut.return_0_str();
+
+            Assert.AreEqual(2, result0str.Count);
+            Assert.AreEqual(0, result0str[0]);
+            Assert.AreEqual("Hi~", result0str[1]);
+        }
+
+        [TestMethod, ExpectedException(typeof(InvocationException))]
+        public void given_lua_script_execute_error_should_throw_invocation_exception()
+        {
+            _sut.raise("Boom!");
         }
     } 
 }
